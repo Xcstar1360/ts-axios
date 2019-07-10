c917c567151cbe20e42df469663f543c59e6368b
Xcstar1360 <1360373992@qq.com>
2019-07-10 21:14:51 +0800

    Implement Interceptor

--- examples/server.js
+++ examples/server.js
@@ -29,6 +29,7 @@ registerSimpleRouter()
 registerBaseRouter()
 registerErrorRouter()
 registerExtendRouter()
+registerInterceptorRouter()
 
 app.use(router)
 
@@ -90,35 +91,35 @@ function registerErrorRouter() {
 }
 
 function registerExtendRouter() {
-    router.get('/extend/get', function(req, res) {
-        res.json({msg: 'hello world'})
+    router.get('/extend/get', function (req, res) {
+        res.json({ msg: 'hello world' })
     })
 
-    router.options('/extend/options', function(req, res) {
+    router.options('/extend/options', function (req, res) {
         res.end()
     })
 
-    router.delete('/extend/delete', function(req, res) {
+    router.delete('/extend/delete', function (req, res) {
         res.end()
     })
 
-    router.head('/extend/head', function(req, res) {
+    router.head('/extend/head', function (req, res) {
         res.end()
     })
 
-    router.post('/extend/post', function(req, res) {
+    router.post('/extend/post', function (req, res) {
         res.json(req.body)
     })
 
-    router.put('/extend/put', function(req, res) {
-        res.json(req.body)
-    })
-    
-    router.patch('/extend/patch', function(req, res) {
+    router.put('/extend/put', function (req, res) {
         res.json(req.body)
     })
 
-    router.get('/extend/user', function(req, res) {
+    router.patch('/extend/patch', function (req, res) {
+        res.json(req.body)
+    })
+
+    router.get('/extend/user', function (req, res) {
         res.json({
             code: 0,
             message: 'ok',
@@ -127,5 +128,11 @@ function registerExtendRouter() {
                 age: 18
             }
         })
+    })
+}
+
+function registerInterceptorRouter() {
+    router.get('/interceptor/get', function (req, res) {
+        res.end('hello')
     })
 }
